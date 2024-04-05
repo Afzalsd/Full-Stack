3f6b250df054e361320e068ec676221dea0b419a
Afzalsd <muhammadafzalsyed2004@gmail.com>
2024-04-05 10:24:45 +0530

    refer source

---
+++ Assignments/class 5/Assignment 5/Assignment5_1.js
@@ -0,0 +1,9 @@
+console.log('Program-1   ->Arithmetic operators')
+let x=100;
+let y=20
+console.log('x =',x,'y =',y)
+console.log('sum =',x+y) 
+console.log('diff =',y-x) 
+console.log('product =',x*y) 
+console.log('quotient =',y/x) 
+console.log('remainder =',y%x) 
