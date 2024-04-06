6f01458031efe82e1df9be9d9d1b6ee9a3e84a5a
Afzalsd <muhammadafzalsyed2004@gmail.com>
2024-04-06 09:29:46 +0530

    practice js

---
+++ Assignments/class 5/Assignment 5/Assignment 6/Assignment6_1.js
@@ -0,0 +1,5 @@
+console.log('Program-1 ---> Largest of 2 numbers')
+let x=3,y=6
+console.log('x =',x,'\ny =',y)
+if(x>y) console.log('x is big')
+else console.log('y is big')
---
+++ Assignments/class 5/Assignment 5/Assignment 6/Assignment6_3.js
@@ -0,0 +1,6 @@
+console.log('Program-3 ---> Factors of a number')
+let target=120
+console.log('number =',target)
+for(let i=1;i<=target;i++){
+    if(target%i==0) console.log(i)
+}
